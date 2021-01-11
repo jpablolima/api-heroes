37abb7ec166d94a51fb216643bf5f2bfe3155f10
jpablolima <jp.pablolima@gmail.com>
2021-01-11 19:46:23 -0300

    change files db.heroes change db.heroes files and list Marvel heroes

--- routes/heroesRoutes/heroes.js
+++ routes/heroesRoutes/heroes.js
@@ -3,21 +3,21 @@ const listHeroes = express.Router()
 const fs = require('fs')
 
 listHeroes.get('/', (req, res) => {
-    const data = fs.readFileSync('./services/db.json')
+    const data = fs.readFileSync('./services/dbHeroes.json')
     const dataHeroesJson = JSON.parse(data)
     res.send(dataHeroesJson)
 })
 
 
 listHeroes.post('/heroes/addnew', (req, res) => {
-    const data = fs.readFileSync('./services/db.json')
+    const data = fs.readFileSync('./services/dbHeroes.json')
     const heroesJson = JSON.parse(data)
     const bodyRequest = req.body
 
 
     heroesJson.heroes.push(bodyRequest)
 
-    fs.writeFileSync('./services/db.json', JSON.stringify(heroesJson))
+    fs.writeFileSync('./services/dbHeroes.json', JSON.stringify(heroesJson))
     res.send('Heroes insert sucess!')
 
 })
@@ -26,7 +26,7 @@ listHeroes.get('/heroes/:id', (req, res) => {
     const idHeroes = req.params.id;
     let heroesSearch = ''
 
-    const data = fs.readFileSync('./services/db.json')
+    const data = fs.readFileSync('./services/dbHeroes.json')
     const heroesJson = JSON.parse(data)
     heroesJson.heroes.map(index => {
         if (index.id == idHeroes) {
@@ -42,11 +42,11 @@ listHeroes.get('/heroes/:id', (req, res) => {
 
 
 
-
-
-
 listHeroes.get('/marvel', (req, res) => {
-    res.send('Marvel')
+    const data = fs.readFileSync('./services/dbHeroesMarvel.json')
+    const dataHeroesJson = JSON.parse(data)
+    res.send(dataHeroesJson)
+
 
 })
 
